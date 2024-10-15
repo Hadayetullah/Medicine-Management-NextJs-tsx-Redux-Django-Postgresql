ba84b85b3cdd43fc2bbcd8496f72f43ec69b1c4a
Hadayetullah <hadayetullah30@gmail.com>
2024-10-15 17:02:32 +0600

    Implement Redux Toolkit

---
+++ frontend/lib/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+
+
+const store = configureStore({
+  reducer: {
+    
+  },
+});
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export default store;
