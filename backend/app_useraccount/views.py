31dfcd1dff89b0fb6e7b88c4d8178761c5c26b7d
Hadayetullah <hadayetullah30@gmail.com>
2024-10-15 14:34:44 +0600

    Create api for registration

---
+++ backend/app_useraccount/views.py
@@ -0,0 +1,32 @@
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
+from .serializers import RegisterSerializer 
+from .models import User
+
+
+''' Create your views here '''
+
+# Generate token manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]  # Allow anyone to access this view
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            email = serializer.data.get('email')
+            return Response({'msg': 'OTP sent to your email.', 'email': email}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
