be947a91730ac3a3b22ba5d073b9ad70cb63b31a
Hadayetullah <hadayetullah30@gmail.com>
2024-10-23 17:05:12 +0600

    Create Company, Category, DosageForm and Medicine models

--- backend/app_product/models.py
+++ backend/app_product/models.py
@@ -1,3 +1,57 @@
+import uuid
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class DosageForm(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand = models.CharField(max_length=255, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    dosage_form = models.ForeignKey(DosageForm, on_delete=models.CASCADE)
+    price = models.FloatField()
+    power = models.FloatField()
+    image = models.ImageField(upload_to='uploads/image', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.brand
+    
+    def medicine_pic_url(self):
+        if self.image:
+            return f'{settings.WEBSITE_URL}{self.image.url}'
+        else:
+            return ''
