deaea76c236c15aa431c1342ccf6c9eb0db037a1
Hadayetullah <hadayetullah30@gmail.com>
2024-11-01 14:10:49 +0600

    Create model SellMedicine

--- backend/app_employee/admin.py
+++ backend/app_employee/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AddMedicine, CreatedBy, UpdatedBy
+from .models import AddMedicine, CreatedBy, UpdatedBy, SellMedicine
 
 # Register your models here.
 admin.site.register(AddMedicine)
 admin.site.register(CreatedBy)
 admin.site.register(UpdatedBy)
+admin.site.register(SellMedicine)
--- backend/app_employee/models.py
+++ backend/app_employee/models.py
@@ -7,18 +7,25 @@ from app_product.models import Medicine
 # Create your models here.
 class CreatedBy(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name='creates_by')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class UpdatedBy(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name='updates_by')
     updated_at = models.DateTimeField(auto_now_add=True)
 
 
 class AddMedicine(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    medicine = models.ForeignKey(Medicine)
-    created_by = models.ForeignKey(CreatedBy)
-    updated_by = models.ForeignKey(UpdatedBy)
+    medicine = models.ForeignKey(Medicine, related_name='added_medicines')
+    created_by = models.ForeignKey(CreatedBy, related_name='created_medicines')
+    updated_by = models.ForeignKey(UpdatedBy, related_name='updated_medicines')
+
+
+class SellMedicine(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    medicine_list = models.ManyToManyField(Medicine, related_name='sells')
+    sold_by = models.ForeignKey(User, related_name='sells_employee')
+    sold_at = models.DateTimeField(auto_now_add=True)
