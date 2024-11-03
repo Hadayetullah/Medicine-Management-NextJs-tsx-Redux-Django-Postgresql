bd13329a524de7b6fc9015e3c48a8f9d70dbd32f
Hadayetullah <hadayetullah30@gmail.com>
2024-11-03 19:48:53 +0600

    Update add-medicine page

--- frontend/app/add-medicine/page.tsx
+++ frontend/app/add-medicine/page.tsx
@@ -12,9 +12,9 @@ export default function AddMedicine() {
     category: "",
     dosage_form: "",
     price: "",
+    power: "",
+    shelf_no: "",
   });
-  const [showOtpModal, setShowOtpModal] = useState(false);
-  const [emailForOtp, setEmailForOtp] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -25,6 +25,7 @@ export default function AddMedicine() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log("Add medicine data: ", formData);
     // const result = await dispatch(registerUser(formData));
     // if (result.meta.requestStatus === "fulfilled") {
     //   // Open OTP modal if registration is successful
@@ -35,7 +36,7 @@ export default function AddMedicine() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-lg">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-lg mt-16">
         <h2 className="text-2xl font-bold text-center text-gray-900">
           Add Medicine
         </h2>
@@ -102,6 +103,40 @@ export default function AddMedicine() {
               type="text"
               name="price"
               value={formData.price}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 mt-1 border rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="power"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Power
+            </label>
+            <input
+              type="text"
+              name="power"
+              value={formData.power}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 mt-1 border rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="shelf_no"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Shelf No.
+            </label>
+            <input
+              type="text"
+              name="shelf_no"
+              value={formData.shelf_no}
               onChange={handleChange}
               required
               className="w-full px-3 py-2 mt-1 border rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
