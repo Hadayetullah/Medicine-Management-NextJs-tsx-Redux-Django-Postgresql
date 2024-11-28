9673a7044ae53f2c403528304d14c9711d915dbf
Hadayetullah <hadayetullah30@gmail.com>
2024-11-28 17:05:47 +0600

    Fix unknown type action value error

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 
 import authReducer from './features/authSlice';
