2cbbef1897a0e5e89cc4a2c3fab77a66c4613ac1
Hadayetullah <hadayetullah30@gmail.com>
2024-11-28 16:53:01 +0600

    Configure store with middleware for the websocketMiddleware to be available the connection globally

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -3,13 +3,21 @@ import { useDispatch } from 'react-redux';
 
 import authReducer from './features/authSlice';
 import employeeReducer from './features/employeeSlice';
+import websocketReducer from './features/websocketSlice';
+
+import { createWebSocketMiddleware } from '@/websocketMiddleware';
+
+
+const websocketMiddleware = createWebSocketMiddleware("ws://127.0.0.1:8000/ws/product/medicine/");
 
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    websocket: websocketReducer,
     employee: employeeReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(websocketMiddleware),
 });
 
 
