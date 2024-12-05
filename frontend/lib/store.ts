0a4d19870004ff44a6736d478c7ff0e197c4aa6b
Hadayetullah <hadayetullah30@gmail.com>
2024-12-06 02:03:55 +0600

    Update some files

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -1,29 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
-import authReducer from './features/authSlice';
-import employeeReducer from './features/employeeSlice';
-import websocketReducer from './features/websocketSlice';
-
-import { createWebSocketMiddleware } from '@/websocketMiddleware';
+import { createWebSocketMiddleware } from "@/websocketMiddleware";
+import { rootReducer } from "./reducers";
 
 
+// Middleware
 const websocketMiddleware = createWebSocketMiddleware();
 
+// Define RootState type
+export type RootState = ReturnType<typeof rootReducer>;
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    websocket: websocketReducer,
-    employee: employeeReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(websocketMiddleware),
-});
+// Define the store factory function
+export const createStore = (preloadedState?: Partial<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(websocketMiddleware),
+    preloadedState,
+  });
 
+// Define AppDispatch type directly from the store
+// const store = createStore(); // Temporary instance to infer types
+// export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = ReturnType<typeof createStore>['dispatch'];
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-
-export default store;
+// Custom hook for dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>();
