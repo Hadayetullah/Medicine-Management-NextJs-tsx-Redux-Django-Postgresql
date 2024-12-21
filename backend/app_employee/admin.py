79ed7bedb0494e8ea8c16c89b053a85a8fed2816
Hadayetullah <hadayetullah30@gmail.com>
2024-12-21 19:33:36 +0600

    Delete an app app_employee

--- backend/app_employee/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AddMedicine, CreatedBy, SellMedicine
-
-# Register your models here.
-# admin.site.register(AddMedicine)
-# admin.site.register(CreatedBy)
-# admin.site.register(SellMedicine)
