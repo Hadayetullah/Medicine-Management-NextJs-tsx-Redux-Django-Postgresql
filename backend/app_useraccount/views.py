5ef1e09dc0e5dacf80748283c88c6ccffe7aa5ae
Hadayetullah <hadayetullah30@gmail.com>
2025-01-12 17:14:21 +0600

    Fix LogoutView in terms of frontend

--- backend/app_useraccount/views.py
+++ backend/app_useraccount/views.py
@@ -81,7 +81,8 @@ class LoginView(APIView):
     
 
 class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def post(self, request):
         try:
