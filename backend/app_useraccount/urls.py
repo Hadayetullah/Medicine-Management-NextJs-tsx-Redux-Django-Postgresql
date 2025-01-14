ffaae06d6384c47e8d6258a8be008b880d27d8dc
Hadayetullah <hadayetullah30@gmail.com>
2025-01-14 19:56:58 +0600

    Update url to refresh token

--- backend/app_useraccount/urls.py
+++ backend/app_useraccount/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, OTPVerifyView, LoginView, LogoutView
+from .views import RegisterView, OTPVerifyView, LoginView, LogoutView, RefreshAccessTokenUnauthUser
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api_register'),
     path('verify-otp/', OTPVerifyView.as_view(), name='api_verify_otp'),
     path('login/', LoginView.as_view(), name='api_login'),
     path('logout/', LogoutView.as_view(), name='api_logout'),
-    # path('refresh/', TokenRefresh.as_view(), name='api_token_refresh'),
+    path('refresh-token/', RefreshAccessTokenUnauthUser.as_view(), name='api_token_refresh'),
     # path('update-token/', TokenRefresh.as_view(), name='api_update_token'),
 ]
