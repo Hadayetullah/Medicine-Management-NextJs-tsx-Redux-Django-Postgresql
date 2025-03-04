404787425793274bef1d8febc2aedfd23c3758f4
Hadayetullah <hadayetullah30@gmail.com>
2025-03-04 21:02:56 +0600

    Create a component

--- frontend/app/components/home/LeftBottomNav.tsx
+++ frontend/app/components/home/LeftBottomNav.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import BottomNav from "./BottomNav";
 
 const LeftBottomNav = () => {
-  return <div className="fixed bottom-[-5px]">LeftBottomNav</div>;
+  return (
+    <div className="absolute w-full bottom-0">
+      <BottomNav />
+    </div>
+  );
 };
 
 export default LeftBottomNav;
