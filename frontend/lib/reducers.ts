dbd936a96d0754d58856d24630de1861590ead6c
Hadayetullah <hadayetullah30@gmail.com>
2025-03-09 19:55:28 +0600

    Create a reducer

--- frontend/lib/reducers.ts
+++ frontend/lib/reducers.ts
@@ -3,10 +3,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import employeeReducer from "./features/employeeSlice";
 import productReducer from "./features/productSlice";
+import customerReducer from "./features/customerSlice";
 
 // Combine reducers
 export const rootReducer = combineReducers({
     auth: authReducer,
     product: productReducer,
     employee: employeeReducer,
+    customer: customerReducer,
 });
