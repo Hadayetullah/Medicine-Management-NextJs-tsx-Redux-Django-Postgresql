8d10a793e8d1c660783280cb0cbbec63a7bd3f1f
Hadayetullah <hadayetullah30@gmail.com>
2025-03-27 00:07:07 +0600

    Fix migrations problem

--- backend/app_sale_product/models.py
+++ backend/app_sale_product/models.py
@@ -5,7 +5,7 @@ from app_product.models import Medicine
 # Create your models here.
 class PrescriptionDetail(models.Model):
     name = models.CharField(max_length=255)
-    age = models.IntegerField()
+    age = models.IntegerField(default=0)
     phone = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
@@ -17,7 +17,7 @@ class PrescriptionDetail(models.Model):
 class PrescribedMedicine(models.Model):
     prescription = models.ForeignKey(PrescriptionDetail, related_name="customer_prescription", on_delete=models.CASCADE)
     medicine = models.ForeignKey(Medicine, related_name="medicine_data", on_delete=models.CASCADE)
-    sold_quantity = models.IntegerField()
+    sold_quantity = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.medicine.name} - (Qty: {self.sold_quantity})"
