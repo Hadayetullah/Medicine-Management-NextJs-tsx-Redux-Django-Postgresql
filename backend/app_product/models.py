af4df09fec1c676adeb408e6c46c7c906f908476
Hadayetullah <hadayetullah30@gmail.com>
2025-04-05 23:49:16 +0600

    Delete some unnecessary models

--- backend/app_product/models.py
+++ backend/app_product/models.py
@@ -72,35 +72,3 @@ class Medicine(models.Model):
         
 
 
-class Customer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    customer_name = models.CharField(max_length=255)
-    customer_age = models.IntegerField()
-    customer_phone = models.CharField(max_length=20, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.customer_name
-
-
-class EditedBy(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='edit_by')
-    updated_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user.name
-
-class SellRecord(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
-    medicine = models.ManyToManyField(Medicine, related_name='medicines')
-    sold_by = models.ForeignKey(User, related_name='soldby', on_delete=models.CASCADE)
-    edited_by = models.ManyToManyField(EditedBy, related_name='editedby')
-    sold_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.customer.customer_name
-        
-
-
